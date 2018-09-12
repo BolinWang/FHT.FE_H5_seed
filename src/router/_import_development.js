d5fbe0aa93bab864cd053d1dc1787b6976f4fe55
王玲波 <454949876@qq.com>
2018-09-12 11:24:03 +0800

    ### *柏林* 删除vue-router

--- src/router/_import_development.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: FT.FE.Bolin
- * @Date: 2018-08-17 14:30:16
- * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-08-17 16:29:16
- */
-
-module.exports = file => require('@/views/' + file + '.vue').default
--- src/router/_import_production.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: FT.FE.Bolin
- * @Date: 2018-08-17 14:30:06
- * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-08-17 14:30:06
- */
-
-module.exports = file => () => import('@/views/' + file + '.vue')
